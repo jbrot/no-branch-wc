8f95d1d47b96ec5859fc10be360564f799f0e29f
Joshua Brot <jbrot@umich.edu>
2020-01-31 02:11:47 -0500

    We now output the character count!
    Implemented a number printing routine, which is pretty complicated.
    Right now it uses one branch, which I will replace eventually once a
    proper solution is found.
    
    I realized the previous solution was still an indirect branch. Ideally,
    I will fix it with self-modifying code---but getting that work is highly
    non-trivial.
    
    In the print_number routine, we can avoid a loop by making the function
    fixed precision---although that's inelegant.
    
    Signed-off-by: Joshua Brot <jbrot@umich.edu>

--- wc.asm
+++ wc.asm
@@ -26,6 +26,10 @@ _main:
     mov rbp, rsp
     sub rsp, 0x08
 
+    ; Set character count to 0
+    xor eax, eax
+    mov [rbp - 4], eax
+
 loop:
     ; Read one char into rbp - 8
     ;
@@ -44,25 +48,76 @@ loop:
     xor r9, r8
     mul r9
     xor rax, r8
+    ; TODO This is an indirect branch. We can eliminate it with self-modifying code.
+    ; However, memory protections will get in the way. I need to figure out how to
+    ; disable said protections.
     jmp rax
 
 continue:
 
-    ; Process the input. For now, just echo to stdout.
+    ; Process the input. For now, just count characters.
 
-    ; user_ssize_t write(int fd, user_addr_t cbuf, user_size_t nbyte);
-    mov rax, 0x2000004
-    mov rdi, 0x01
-    lea rsi, [rbp - 8]
-    mov rdx, 0x01
-    syscall
+    ; Increase character count
+    mov eax, [rbp - 4]
+    inc eax
+    mov [rbp - 4], eax
 
     ; Repeat
     jmp loop
 
 done:
 
+    ; print character count
+    xor rsi, rsi
+    mov esi, [rbp - 4]
+    call print_number
+
     ; exit(0)
     mov rax, 0x2000001
-    mov rdi, 0x00
+    mov rdi, [rbp - 4]
     syscall
+
+; Print the number in rsi out to the console.
+print_number:
+    push rbp
+    mov rbp, rsp
+
+    ; r9: current value
+    ; r11: length
+    mov r9, rsi
+    xor r11, r11
+
+pn_loop:
+    ; Increase length, reserve space for next character
+    sub rsp, 0x01
+    inc r11
+
+
+    ; Divide parameter by 10, store remainder at esp
+    mov rax, r9 ; rax: current value
+    mov r8, 0x0A ; r8: 10
+    div r8
+    mov r10, rax ; r10: current value divided by 10
+    mul r8
+    sub r9, rax ; r9: modulus
+    add r9, '0'
+    mov byte [rsp], r9b ; store modulus as ascii character in buffer
+    mov r9, r10 ; Set current value (r9) to itself divided by 10 (r10)
+
+    ; If we're at 0, we're done!
+    ; TODO: Avoid branching here using whatever solution we come up with above.
+    cmp r9, 0x00
+    jne pn_loop
+
+    ; Write out the buffer
+    ; user_ssize_t write(int fd, user_addr_t cbuf, user_size_t nbyte);
+    mov rax, 0x2000004
+    mov rdi, 0x01
+    lea rsi, [rsp]
+    mov rdx, r11
+    syscall
+
+    ; Restore stack frame, and return.
+    mov rsp, rbp
+    pop rbp
+    ret
